78fd9c16db2753acfa2ef73fcfdd4bc11bc2af50
Michaela <botka@email.cz>
2023-09-12 10:17:39 +0200

    Miniprojekt - dvojitá úvodní stránka - html, css, script

--- script.js
+++ script.js
@@ -7,34 +7,31 @@ const slidesLength = slideRight.querySelectorAll("div").length;
 
 let numberActiveSlide = 0;
 
-slideLeft.style.top = `-${(slidesLength-1)*100}vh`;
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
-arrowUp.addEventListener("click",function(){
+arrowUp.addEventListener("click", function () {
     changeSlide("up");
 });
 
-arrowDown.addEventListener("click",function(){
+arrowDown.addEventListener("click", function () {
     changeSlide("down");
 });
 
-function changeSlide(direction){
+function changeSlide(direction) {
     const sliderHeight = containerSlider.clientHeight;
 
-    if (direction === "up"){
+    if (direction === "up") {
         numberActiveSlide++;
-        if (numberActiveSlide>slidesLength-1){
-            numberActiveSlide=0;
+        if (numberActiveSlide > slidesLength - 1) {
+            numberActiveSlide = 0;
         }
-
     } else {
         numberActiveSlide--;
-        if (numberActiveSlide<0){
-            numberActiveSlide = slidesLength-1;
+        if (numberActiveSlide < 0) {
+            numberActiveSlide = slidesLength - 1;
         }
-    }  
-    
+    }
+
     slideRight.style.transform = `translateY(-${numberActiveSlide * sliderHeight}px)`;
     slideLeft.style.transform = `translateY(${numberActiveSlide * sliderHeight}px)`;
-
 };
-
